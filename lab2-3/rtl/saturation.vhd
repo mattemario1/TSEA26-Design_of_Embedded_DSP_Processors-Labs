9ee453758cc46b491136e19d988fcf4bb6033aa0
matho019 <matho019@su03-202.ad.liu.se>
2025-11-28 16:51:33 +0100

    Added saturation for lab 2

--- lab2-3/rtl/saturation.vhd
+++ lab2-3/rtl/saturation.vhd
@@ -12,10 +12,33 @@ entity saturation is
 end saturation;
 
 architecture saturation_rtl of saturation is
+
+  constant ZERO_8_BITS : signed(7 downto 0) := (others => '0');
+  constant ONE_8_BITS  : signed(7 downto 0) := (others => '1');
+  constant MAX_VALUE   : signed(39 downto 0) := 0b00000000011111111111111111111111111111111;
+  constant MIN_VALUE   : signed(39 downto 0) := 0b11111111100000000000000000000000000000000;
+
 begin  -- saturation_rtl
 
-  -- Remove the following lines and put your code here
-  value_o <= value_i;
-  did_sat_o <= '0';
-  
+  signal sign_bit <= value_i(39);
+  signal guard_bits <= value_i(38 downto 32);
+
+
+  case do_sat_i is
+    when "0" =>
+      value_o <= value_i
+      did_sat_o <= "0"
+    when "1" =>
+      if (sign_bit = "0") and (guard_bits /= ZERO_8_BITS) then
+        value_o <= MAX_VALUE;
+        did_sat_o <= "1";
+      elsif (sign_bit = "1") and (guard_bits /= ONE_8_BITS) then
+        value_o <= MIN_VALUE;
+        did_sat_o <= "1";
+      else
+        value_o <= value_i;
+        did_sat_o <= "0";
+      end if;
+  end case;
+
 end saturation_rtl;
