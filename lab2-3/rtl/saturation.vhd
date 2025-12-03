383840b558ba5ad8c4d2589feab728a5e76a8bce
matho019 <matho019@muxen2-103.ad.liu.se>
2025-12-03 19:10:27 +0100

    Lab2 saturation works

--- lab2-3/rtl/saturation.vhd
+++ lab2-3/rtl/saturation.vhd
@@ -12,33 +12,29 @@ entity saturation is
 end saturation;
 
 architecture saturation_rtl of saturation is
-
-  constant ZERO_8_BITS : signed(7 downto 0) := (others => '0');
-  constant ONE_8_BITS  : signed(7 downto 0) := (others => '1');
-  constant MAX_VALUE   : signed(39 downto 0) := 0b00000000011111111111111111111111111111111;
-  constant MIN_VALUE   : signed(39 downto 0) := 0b11111111100000000000000000000000000000000;
-
 begin  -- saturation_rtl
 
-  signal sign_bit <= value_i(39);
-  signal guard_bits <= value_i(38 downto 32);
 
-
+process (do_sat_i, value_i) begin
   case do_sat_i is
-    when "0" =>
-      value_o <= value_i
-      did_sat_o <= "0"
-    when "1" =>
-      if (sign_bit = "0") and (guard_bits /= ZERO_8_BITS) then
-        value_o <= MAX_VALUE;
-        did_sat_o <= "1";
-      elsif (sign_bit = "1") and (guard_bits /= ONE_8_BITS) then
-        value_o <= MIN_VALUE;
-        did_sat_o <= "1";
+    when '0' =>
+      value_o <= value_i;
+      did_sat_o <= '0';
+    when '1' =>
+      if (value_i(38 downto 31) /= "00000000" and value_i(39) = '0') then  --Saturate to Max Value.
+        value_o <= x"007FFFFFFF";
+        did_sat_o <= '1';
+      elsif (value_i(38 downto 31) /= "11111111" and value_i(39) = '1') then      --Saturate to max value.
+        value_o <= x"ff80000000";
+        did_sat_o <= '1';
       else
         value_o <= value_i;
-        did_sat_o <= "0";
+        did_sat_o <= '0';
       end if;
+    when others => 
+      value_o <= value_i;
+      did_sat_o <= '0';
   end case;
+end process;
 
 end saturation_rtl;
